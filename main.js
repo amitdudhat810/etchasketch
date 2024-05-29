50cd2b7b696a9f3b7cdb215b98caa52f4aee00ae
Amit Dudhat <166971850+amitdudhat810@users.noreply.github.com>
2024-05-29 15:39:51 -0400

    Initial Development

---
+++ main.js
@@ -0,0 +1,9 @@
+let board = document.querySelector('.board')
+// document.getElementsByClassName("board").
+board.setAttribute("style", "color: blue; background: pink;grid-template-columns: repeat(16, 1fr);grid-template-rows: repeat(16, 1fr)")
+// board.setAttribute("style","grid-template-columns: repeat(16, 1fr)")
+// board.setAttribute("style","grid-template-rows: repeat(16, 1fr)")
+
+
+// board.style.background = "blue"
+
